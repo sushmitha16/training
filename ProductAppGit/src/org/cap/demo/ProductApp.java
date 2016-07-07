1b6c5a47415d352886e406a9839f760c810e825d
Training <Training@DIN39000555.corp.capgemini.com>
2016-07-07 10:24:45 +0530

    updated

--- ProductAppGit/src/org/cap/demo/ProductApp.java
+++ ProductAppGit/src/org/cap/demo/ProductApp.java
@@ -2,9 +2,15 @@ package org.cap.demo;
 
 public class ProductApp {
 
+	
+	int[] arr = {1, 0, 16};
 	public void productDisplay() {
 		System.out.println("productDisplay..");
 
+		
+		for (int i=0; i<arr.length;i++){
+			System.out.println("Data: ");
+		}
 	}
 
 }
