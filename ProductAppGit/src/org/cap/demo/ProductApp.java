ee3762f3af49a4ce2b23e064ba6e7cc020d1dd9d
Training <Training@DIN39000555.corp.capgemini.com>
2016-07-07 10:58:07 +0530

    foreach loop

--- ProductAppGit/src/org/cap/demo/ProductApp.java
+++ ProductAppGit/src/org/cap/demo/ProductApp.java
@@ -5,11 +5,10 @@ public class ProductApp {
 	
 	int[] arr = {1, 0, 16};
 	public void productDisplay() {
-		//System.out.println("productDisplay..");
-
+		
 		System.out.println("Data: ");
-		for (int i=0; i<arr.length;i++){
-			System.out.println("Array value: "+arr[i]);
+		for (int i:arr){
+			System.out.println("Array value: "+i);
 		}
 	}
 
